e1cde58ce5e9cc998e6ac3baa56c82ddc4511e50
Andre Santiago <alfsantiago96@gmail.com>
2021-03-12 19:45:42 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,14 @@
 package com.educandoweb.course.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name =   "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -17,6 +19,14 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
